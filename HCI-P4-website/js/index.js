9371fdeb84afddddbf01d98a99f84a19a7c663b1
mangorocoro <daniel01001000@gmail.com>
2019-11-30 23:00:12 -0500

    removed old

--- HCI-P4-website/js/index.js
+++
@@ -1,8 +0,0 @@
-var mainText = document.getElementById("mainText")
-var submitBtn = document.getElementById("submitBtn")
-
-function submitClick(){
-    var firebaseRef = firebase.databae();
-    window.alert("testing")
-    firebaseRef.child("Text").set("hellp");
-}
